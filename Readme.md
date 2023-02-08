fd3836fd5452b984a5e4ffb7d2754498e2fd6647
Marco <antoniohdez888@gmail.com>
2023-02-08 14:48:38 -0600

    Backend 1.0 terminado

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Backend MERN - Calendar  
+
+Backend que se creó para un curso de React
+
+Marco Antonio Hernández González
---
+++ helpers/jwt.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const generarJWT = ( uid, name ) => {
+
+    const payload = { uid, name };
+
+    const token = jwt.sign( payload, process.env.SECRET_JWT_SEED, {
+        expiresIn: '2h'
+    });
+
+    if( !token ){
+        return 'No se pudo generar el token'
+    }
+    return token;
+
+}
+
+module.exports = {
+    generarJWT
+}
